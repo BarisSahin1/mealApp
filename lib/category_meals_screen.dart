1036609d3467dc83f6849ecab8e040254ee17d11
BarisSahin <sahinbarisdev@gmail.com>
2022-07-04 15:37:58 +0300

    Named route configurations are added

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -5,13 +5,16 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
+  // final String categoryId;
+  // final String categoryTitle;
 
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
